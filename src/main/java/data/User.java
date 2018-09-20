67bd7b99eaa505093b24807936ac2e7478565040
mf226 <cph-mf226@cphbusiness.dk>
2018-09-20 12:59:00 +0200

    created user

--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -1,5 +1,50 @@
 package data;
 
 public class User {
+    private String username;
+    private String password;
+    private int balance;
+    
+    public User() {
+        
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    
+    public boolean withdraw(int price) {
+        if(balance >= price){
+            this.balance -= price;
+            return true;
+        }
+        return false;
+    }
+    
     
 }
