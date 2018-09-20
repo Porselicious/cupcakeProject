9396111235151ab70f62249a7095941f9fbd8fc1
Jonatan Hjelm <cph-jh409@cphbusiness.dk>
2018-09-20 13:15:24 +0200

    update dummyDB

--- dummyData.sql
+++ dummyData.sql
@@ -1,5 +1,5 @@
 insert into usersDB.user
-values('hans123', 'hotdog45'), ('davsdu', 'nejtakdu');
+values('hans123', 'hotdog45', 'mad@had.dk'), ('davsdu', 'nejtakdu', '123@hejsa.dk');
 
 insert into usersDB.balance
 values(50, 'hans123'), (100, 'davsdu');
