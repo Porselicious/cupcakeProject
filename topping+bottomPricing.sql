378ba3087ab099f966dadc676f0431d5c8a28a58
Porselicious <porseg69@gmail.com>
2018-09-21 10:28:53 +0200

    Topping data

---
+++ topping+bottomPricing.sql
@@ -0,0 +1,19 @@
+use usersDB;
+
+insert into usersDB.bottom(bottomName, price)
+values ('Chocolate', 5),
+('Vanilla', 5),
+('Nutmeg', 5),
+('Pistacio', 6),
+('Almond', 7);
+
+insert into usersDB.topping(toppingName, price)
+values ('Chocolate', 5),
+('Blueberry', 5),
+('Raspberry', 5),
+('Crispy', 6),
+('Strawberry', 6),
+('Rum/Raisin', 7),
+('Orange', 8),
+('Lemon', 8),
+('Blue Cheese', 9);
